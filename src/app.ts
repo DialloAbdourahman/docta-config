9f36f6a7414a0bf8ce1da80f70e9d87913d55fe7
Diallo Abdourahman <dialliabdourahman78@gmail.com>
2025-12-22 21:24:32 +0100

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
-import { errorHandler } from "docta-package";
+import { errorHandler, logger } from "docta-package";
 import periodRouter from "./routers/periodRouter";
 import doctorRouter from "./routers/doctorRouter";
 import adminRouter from "./routers/adminRouter";
@@ -11,6 +11,22 @@ import { swaggerSpec } from "./swagger";
 const app = express();
 
 app.use(express.json());
+
+// Request logger
+app.use((req, res, next) => {
+  logger.info(`Request: ${req.method} ${req.url}`, {
+    method: req.method,
+    route: req.path,
+    baseUrl: req.baseUrl,
+    originalUrl: req.originalUrl,
+    params: req.params,
+    query: req.query,
+    body: req.body,
+    headers: req.headers,
+    ip: req.ip,
+  });
+  next();
+});
 
 // Swagger documentation route
 app.use("/api/config/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
